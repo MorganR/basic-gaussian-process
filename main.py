0f20ee0822138968dca6dc49ba4b92b81d853916
MorganR <roffmorgan@gmail.com>
2017-01-14 17:56:58 -0500

    Generate some sample data

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world.\n")
+import numpy as np
+import matplotlib.pyplot as plt
+
+# generate sample data
+x_data = np.linspace(-5, 5, 20)
+y_data = np.random.normal(0.0, 1.0, x_data.size)
+
+plt.plot(x_data, y_data, 'o')
+plt.show()
