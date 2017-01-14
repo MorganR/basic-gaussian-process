4932c3bb3e7f1ab3bda5f1efb1bb0703a99f2c88
MorganR <roffmorgan@gmail.com>
2017-01-14 18:46:38 -0500

    Add covariance comment

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ plt.show()
 mean_est = tf.Variable(0.0)
 # Use squared exponential covariance matrix
 x_rows, x_cols = tf.meshgrid(x_data, x_data)
+# Covariance defined as $exp(-0.5*(x_i-x_j)^2/l^2)$ where l is the length-scale
 covariance_est = tf.exp(tf.scalar_mul(-0.5, tf.squared_difference(x_rows, x_cols)))
 
 sess = tf.Session()
