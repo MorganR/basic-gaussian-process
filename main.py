0c17311f7fd511f5dae8f8e4acc2dce1a2de3cf5
MorganR <roffmorgan@gmail.com>
2017-01-14 18:03:11 -0500

    Change to sin() function with noise

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 # generate sample data
-x_data = np.linspace(-5, 5, 20)
-y_data = np.random.normal(0.0, 1.0, x_data.size)
+x_data = np.linspace(-math.pi, math.pi, 30)
+y_data = np.sin(x_data) + np.random.normal(0.0, 0.1, x_data.size)
 
 plt.plot(x_data, y_data, 'o')
 plt.show()
+
